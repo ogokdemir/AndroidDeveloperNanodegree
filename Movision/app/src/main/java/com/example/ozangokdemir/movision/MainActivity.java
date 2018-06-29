6628a95e190a0d1b11696d2c7c57017f9068eb6b
ogokdemir <ozan.gokdemir15@ncf.edu>
2018-06-29 02:19:32 -0400

    Working version of the network connectivity checking. App doesn't crash in the lack of connection now.

--- Movision/app/src/main/java/com/example/ozangokdemir/movision/MainActivity.java
+++ Movision/app/src/main/java/com/example/ozangokdemir/movision/MainActivity.java
@@ -24,48 +24,113 @@ import org.parceler.Parcels;
 
 import java.util.concurrent.ExecutionException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity implements MovieItemClickListener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private String mChoosenSortParameter;
-    private RecyclerView recyclerView;
-    private FetchMovieDataTask asyncTask;
+    @BindView(R.id.rw_grid_container) RecyclerView recyclerView;
+    FetchMovieDataTask asyncTask;
     private Movie[] currentMovieList;
     private MovieAdapter adapter;
-    private GridLayoutManager gridLayoutManager;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        recyclerView = (RecyclerView)findViewById(R.id.rw_grid_container);
-        mChoosenSortParameter = getString(R.string.sort_by_default); // default sort criteria is top rated movies.
+        mChoosenSortParameter = getString(R.string.sort_by_default);
+        setup();
 
-
-         asyncTask = new FetchMovieDataTask(getResources().getString(R.string.api_key));
-
-         gridLayoutManager = new GridLayoutManager(this, 2);
-
-
-      //  try {
-
-            currentMovieList = executeAsynctaskWithNetworkCheck(asyncTask);
-            adapter = new MovieAdapter(currentMovieList,this);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setLayoutManager(gridLayoutManager);
-            recyclerView.setHasFixedSize(true);
-
-            /*
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-*/
     }
+
+
+    private void refreshTheRecyclerView(){
+
+    }
+
+    private void setup(){
+        asyncTask = new FetchMovieDataTask(getResources().getString(R.string.api_key));
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        currentMovieList = executeAsynctaskWithNetworkCheck(asyncTask);
+
+        if(currentMovieList != null) // if there was connection and some movies could be fetched:
+            adapter = new MovieAdapter(currentMovieList, this);
+        else
+            return;
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.rw_grid_container);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setHasFixedSize(true);
+    }
+
+
+
+
+    /**
+     * This method updates the current sorting criteria by instantiating a new asynctasks object and making a new api call.
+     *
+     * Called when the user picks a different sorting from the option menu.
+     *
+     * @param choosenSortParameter The sorting criteria that the user picks from the drop down action bar menu.
+     */
+    private void updateCurrentMovieList(String choosenSortParameter){
+
+        if(isOnline()) {
+
+            setContentView(R.layout.activity_main);
+
+            setup();
+
+        }else{
+            notifyUserOfNoInternetConnection();
+            return;
+        }
+
+        //Updating the adapter's data source with the freshly updated Movie[] array.
+        adapter.updateAdapterDataSource(currentMovieList);
+
+        //setting the adapter of the recyclerview changes what user sees on the screen.
+        recyclerView.setAdapter(adapter);
+
+
+
+    }
+
+    private Movie[] executeAsynctaskWithNetworkCheck(FetchMovieDataTask task){
+
+        Movie[] movieList = null;
+
+        if(isOnline()) {
+            try {
+
+                movieList = task.execute(mChoosenSortParameter).get();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+
+
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+
+            }
+
+            // Device doesn't have network connection.
+        }
+        else
+            notifyUserOfNoInternetConnection();
+
+        return movieList;
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,6 +139,13 @@ public class MainActivity extends AppCompatActivity implements MovieItemClickLis
         return true;
 
     }
+
+
+    /**
+     *
+     * @param item Options menu item that was clicked. These items are sorting criteria (popular, top rated etc.)
+     *
+     */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -109,60 +181,25 @@ public class MainActivity extends AppCompatActivity implements MovieItemClickLis
         return super.onOptionsItemSelected(item);
     }
 
-
     /**
-     * This method updates the current sorting criteria by instantiating a new asynctasks object and making a new api call.
+     * This elegant piece of code checks the network connectivity. Source: stackoverflow.com
      *
-     * Called when the user picks a different sorting from the option menu.
-     *
-     * @param choosenSortParameter The sorting criteria that the user picks from the drop down action bar menu.
+     * @return State of connectivity - whether the device has network connection.
      */
-    private void updateCurrentMovieList(String choosenSortParameter){
 
-        if(isOnline()) {
-            setContentView(R.layout.activity_main);
-
-
-            GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
-
-            asyncTask = new FetchMovieDataTask(getString(R.string.api_key));
-
-            recyclerView = (RecyclerView) findViewById(R.id.rw_grid_container);
-            recyclerView.setLayoutManager(layoutManager);
-            currentMovieList = executeAsynctaskWithNetworkCheck(asyncTask);
-
-            adapter = new MovieAdapter(currentMovieList, this);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setHasFixedSize(true);
-
-            //try {
-
-
-
-        }else{
-            Toast.makeText(getApplicationContext(), getString(R.string.no_network_connection_error), Toast.LENGTH_LONG)
-                    .show();
-
-            setContentView(R.layout.error_layout);
-        }
-        /*
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Log.d(TAG, "Asynctask HTTP GET Request was interrupted!");
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-*/
-        //Updating the adapter's data source with the freshly updated Movie[] array.
-        adapter.updateAdapterDataSource(currentMovieList);
-
-        //setting the adapter of the recyclerview changes what user sees on the screen.
-        recyclerView.setAdapter(adapter);
-
-
-
+    private boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
+
+    /**
+     * Implementation of the listener for the movie poster clicks.
+     *
+     * @param movieItemIdx index of the movie poster that was tapped.
+     */
     @Override
     public void onMovieItemClick(int movieItemIdx) {
 
@@ -173,52 +210,17 @@ public class MainActivity extends AppCompatActivity implements MovieItemClickLis
         toDetailActivity.putExtras(bundle);
         startActivity(toDetailActivity);
 
-        }
+    }
 
     /**
-     * This elegant piece of code checks the network connectivity. Source: stackoverflow.com
-     *
-     * @return State of connectivity - whether the device has network connection.
+     * Helper method for notifying the user that their device is not connected to the internet.
      */
-    private boolean isOnline() {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
+
+    private void notifyUserOfNoInternetConnection(){
+        Toast.makeText(getApplicationContext(), getString(R.string.no_network_connection_error), Toast.LENGTH_LONG)
+                .show();
+
+        setContentView(R.layout.error_layout);
     }
 
-    private Movie[] executeAsynctaskWithNetworkCheck(FetchMovieDataTask task){
-
-        Movie[] movieList = null;
-
-        if(isOnline()) {
-            try {
-                movieList = task.execute(mChoosenSortParameter).get();
-
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-
-
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-
-            }
-        }else{
-            Toast.makeText(getApplicationContext(), getString(R.string.no_network_connection_error), Toast.LENGTH_LONG)
-                    .show();
-
-            setContentView(R.layout.error_layout);
-        }
-
-        return movieList;
-    }
-
-/*
-    private boolean isCurrentlyOnTheErrorScreen(){
-
-        boolean this.getWindow().getDecorView().findViewById(android.R.id.content).equals(R.layout.activity_main);
-
-    }
-*/
 }
