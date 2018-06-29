9b6a5c783660655033306299231531a32d29cc72
ogokdemir <ozan.gokdemir15@ncf.edu>
2018-06-28 22:45:09 -0400

    DetailActivity UI design and inflation complete. Landscape mode also added.

--- Movision/app/src/main/java/com/example/ozangokdemir/movision/DetailActivity.java
+++ Movision/app/src/main/java/com/example/ozangokdemir/movision/DetailActivity.java
@@ -3,27 +3,61 @@ package com.example.ozangokdemir.movision;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.accessibility.AccessibilityManager;
-import android.widget.Toast;
-
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 import org.parceler.Parcels;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailActivity extends AppCompatActivity {
 
+    @BindView (R.id.iw_detail_activity_poster) ImageView mMoviePosterIw;
+    @BindView(R.id.tw_detail_activity_rating) TextView mAvgRatingTw;
+    @BindView(R.id.tw_detail_activity_release_date) TextView mReleaseDataTw;
+    @BindView(R.id.tw_detail_activity_overview) TextView mOverViewTw;
+    @BindView(R.id.tw_detail_activity_title) TextView mTitleTw;
 
     //Extras that are sent to this activity will be passed and retrieved with this key.
     public static final String MOVIE_INTENT_KEY = "Detail Activity Mail Box";
 
-    private static final String TAG = DetailActivity.class.getSimpleName();
+    private static final String TAG = DetailActivity.class.getSimpleName(); // for debugging purposes.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Intent intent = getIntent();
-        Movie movie = (Movie) Parcels.unwrap(intent.getParcelableExtra(MOVIE_INTENT_KEY));
-        Toast.makeText(this, movie.getmTitle(), Toast.LENGTH_SHORT).show();
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent(); // will never be null.
+        Movie movie = Parcels.unwrap(intent.getParcelableExtra(MOVIE_INTENT_KEY));
+
+        displayMovieData(movie);
 
     }
+
+    /*
+        Inflate the UI with data of the selected movie.
+     */
+
+    private void displayMovieData(Movie movie){
+
+        // Display the movie poster.
+        Picasso.with(this)
+                .load(movie.getmPosterUri())
+                .error(R.drawable.error)
+                .placeholder(R.drawable.loading)
+                .into(mMoviePosterIw);
+
+        // Bind data to other text views in the layout.
+
+        mAvgRatingTw.append("\n"+movie.getmAverageRating()+"/10");
+        mReleaseDataTw.append("\n"+movie.getmReleaseDate());
+        mTitleTw.setText(movie.getmTitle());
+        mOverViewTw.setText(movie.getmOverview());
+
+    }
+
 }
